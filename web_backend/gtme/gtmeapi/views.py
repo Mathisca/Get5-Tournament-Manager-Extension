82ed7e75e004a447e41e69eb0e2c4125a4d11f33
Mathis Cariou <contact@mathisca.fr>
2019-08-12 22:42:13 +0200

    install rest api and social auth

--- web_backend/gtme/gtmeapi/views.py
+++ web_backend/gtme/gtmeapi/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+
+from rest_framework.permissions import IsAuthenticated
+
+
+def sample_view(request):
+    current_user = request.user
+    return HttpResponse(current_user.is_authenticated)
