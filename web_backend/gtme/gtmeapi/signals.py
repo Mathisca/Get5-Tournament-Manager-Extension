7a060d83ca6393019bf3bdfc99147af8af44548f
Mathis Cariou <contact@mathisca.fr>
2019-08-13 15:11:34 +0200

    steam dump account info

--- web_backend/gtme/gtmeapi/signals.py
+++
@@ -1,7 +0,0 @@
-from allauth.account.signals import user_logged_in
-from django.dispatch import receiver
-
-
-@receiver(user_logged_in)
-def social_login_fname_lname_profilepic(sociallogin, user):
-    pass
